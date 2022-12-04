c0e75f1234bcf4fb981a3a16c23187f9da499d7f
Cemil Aslan <you@example.com>
2022-12-04 22:06:05 +0100

    added repository, tested the adding

--- src/main/java/at/spengergasse/studentzeugnis/model/StudentZeugnis.java
+++ src/main/java/at/spengergasse/studentzeugnis/model/StudentZeugnis.java
@@ -12,20 +12,34 @@ import java.time.LocalDate;
 @Getter
 
 @Data
-@Document("studentZeugnis")
+@Document("students_zeugnis")
 public class StudentZeugnis {
-    private String version;
     @Id
     private int id;
-    private boolean aufstieg;
+    private String version;
+    private boolean passed;
     private String firstname;
     private String lastname;
     private Gender gender;
     private LocalDate brithDate;
     private double age;
     private WP WP;
-    private Subject subject;
+    private List<Subject> subject;
     private LocalDate timeStamp;
+
+    public StudentZeugnis(int id,String version, boolean passed, String firstname, String lastname, Gender gender, LocalDate brithDate, double age, WP WP, List<Subject> subject, LocalDate timeStamp) {
+        this.id = id;
+        this.version = version;
+        this.passed = passed;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.brithDate = brithDate;
+        this.age = age;
+        this.WP = WP;
+        this.subject = subject;
+        this.timeStamp = timeStamp;
+    }
 
 
 }
--- src/main/java/at/spengergasse/studentzeugnis/model/Subject.java
+++ src/main/java/at/spengergasse/studentzeugnis/model/Subject.java
@@ -1,7 +1,12 @@
 package at.spengergasse.studentzeugnis.model;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Subject {
     private String name;
     private String WS;
     private String SS;
+
+
 }
